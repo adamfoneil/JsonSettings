6a514c094841f329af4343ed37effbcbedb0b56f
Adam O'Neil <adamosoftware@gmail.com>
2018-07-06 14:12:51 -0400

    added nuspec

--- JsonSettings/Properties/AssemblyInfo.cs
+++ JsonSettings/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonSettings")]
-[assembly: AssemblyDescription("Json Application Settings")]
+[assembly: AssemblyTitle("Json Settings")]
+[assembly: AssemblyDescription("Json App and User Settings")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Adam O'Neil Software")]
 [assembly: AssemblyProduct("JsonSettings")]
