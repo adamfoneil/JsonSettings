33aebddeba586ce402a02f83ab94574aae86b550
Adam O'Neil <adamosoftware@gmail.com>
2019-02-24 10:20:02 -0500

    version 1.0.9 new optional args on Save and Load methods

--- JsonSettings.Library/JsonFile.cs
+++ JsonSettings.Library/JsonFile.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -6,7 +7,7 @@ namespace JsonSettings
 {
 	public static class JsonFile
 	{
-		public static void Save<T>(string fileName, T @object)
+		public static void Save<T>(string fileName, T @object, Action<JsonSerializerSettings> customizeSettings = null)
 		{
 			string folder = Path.GetDirectoryName(fileName);
 			if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
@@ -18,12 +19,15 @@ namespace JsonSettings
 					Formatting = Formatting.Indented,
 					ContractResolver = new DataProtectionResolver()
 				};
+
+				customizeSettings?.Invoke(settings);
+
 				string json = JsonConvert.SerializeObject(@object, settings);
 				writer.Write(json);
 			}
 		}
 
-		public async static Task SaveAsync<T>(string fileName, T @object)
+		public async static Task SaveAsync<T>(string fileName, T @object, Action<JsonSerializerSettings> customizeSettings = null)
 		{
 			string folder = Path.GetDirectoryName(fileName);
 			if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
@@ -35,13 +39,18 @@ namespace JsonSettings
 					Formatting = Formatting.Indented,
 					ContractResolver = new DataProtectionResolver()
 				};
+
+				customizeSettings?.Invoke(settings);
+
 				string json = JsonConvert.SerializeObject(@object, settings);
 				await writer.WriteAsync(json);
 			}
 		}
 
-		public static T Load<T>(string fileName)
+		public static T Load<T>(string fileName, Func<T> ifNotExists = null)
 		{
+			if (!File.Exists(fileName) && ifNotExists != null) return ifNotExists.Invoke();
+
 			using (StreamReader reader = File.OpenText(fileName))
 			{
 				JsonSerializerSettings settings = new JsonSerializerSettings()
@@ -58,8 +67,10 @@ namespace JsonSettings
 			return default(T);
 		}
 
-		public async static Task<T> LoadAsync<T>(string fileName)
+		public async static Task<T> LoadAsync<T>(string fileName, Func<T> ifNotExists = null)
 		{
+			if (!File.Exists(fileName) && ifNotExists != null) return ifNotExists.Invoke();
+
 			using (StreamReader reader = File.OpenText(fileName))
 			{
 				JsonSerializerSettings settings = new JsonSerializerSettings()
