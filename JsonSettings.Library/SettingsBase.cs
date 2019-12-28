8680787e4d4626ef53f5cd1a21901090f12118fd
Adam ONeil <adamosoftware@gmail.com>
2019-12-28 10:39:29 -0500

    added SettingsBase

---
+++ JsonSettings.Library/SettingsBase.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JsonSettings.Library
+{
+    public abstract class SettingsBase
+    {
+        [JsonIgnore]
+        public abstract string Filename { get; }
+
+        public static T Load<T>() where T : SettingsBase, new()
+        {
+            T result = new T();
+
+            if (File.Exists(result.Filename))
+            {
+                result = JsonFile.Load<T>(result.Filename);
+            }
+
+            return result;
+        }
+
+        public void Save(Action<JsonSerializerSettings> updateSettings = null)
+        {
+            JsonFile.Save(Filename, updateSettings);
+        }
+    }
+}
